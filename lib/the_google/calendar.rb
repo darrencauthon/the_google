166bdeb96a792bff89721da16d5837af13136cca
Darren Cauthon <darren@cauthon.com>
2015-02-26 11:09:54 -0600

    Spacing.

--- lib/the_google/calendar.rb
+++ lib/the_google/calendar.rb
@@ -1,6 +1,9 @@
 module TheGoogle
+
   class Calendar
+
     attr_accessor :name, :access_role, :id
+
     def self.build_all_from api_output
       api_output.data.items.map do |item|
         new.tap do |c|
@@ -10,5 +13,7 @@ module TheGoogle
         end
       end
     end
+
   end
+
 end
