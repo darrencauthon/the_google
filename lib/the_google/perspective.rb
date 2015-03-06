92bbb36009ee476a7d52170d08c591301ca3f03c
Darren Cauthon <darren@cauthon.com>
2015-03-06 09:27:03 -0600

    Formatting.

--- lib/the_google/perspective.rb
+++ lib/the_google/perspective.rb
@@ -10,11 +10,11 @@ module TheGoogle
 
     def client
       Google::APIClient.new.tap do |a|
-        a.authorization.client_id = config[:client_id]
+        a.authorization.client_id     = config[:client_id]
         a.authorization.client_secret = config[:client_secret]
-        a.authorization.scope = config[:scope]
+        a.authorization.scope         = config[:scope]
         a.authorization.refresh_token = config[:refresh_token]
-        a.authorization.access_token = config[:access_token]
+        a.authorization.access_token  = config[:access_token]
         if a.authorization.refresh_token && a.authorization.expired?
           a.authorization.fetch_access_token!
         end
