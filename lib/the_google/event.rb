ec9c3d4f0e3dfc95418e2fa693659a8c6eca76d5
Darren Cauthon <darren@cauthon.com>
2015-06-03 16:14:23 -0500

    Refactor.

--- lib/the_google/event.rb
+++ lib/the_google/event.rb
@@ -10,8 +10,24 @@ module TheGoogle
 
     def self.apply_recurrence event, timeframe
       return [event] if event.recurrence.nil? || event.recurrence.empty?
-      lookup_recurring_dates(date: event.start, recur: event.recurrence[0])
-        .map { |d| event.dup.tap { |e| e.start = d; e.end = d + (event.end - event.start) } }
+      dates_for(event).map { |d| build_a_new_event_for event, d, timeframe }
+    end
+
+    class << self
+
+      private
+
+      def dates_for event
+        lookup_recurring_dates date: event.start, recur: event.recurrence[0]
+      end
+
+      def build_a_new_event_for event, date, timeframe
+        event.dup.tap do |new_event|
+          new_event.start = date
+          new_event.end   = date + (event.end - event.start)
+        end
+      end
+
     end
 
   end
